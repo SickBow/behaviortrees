1350eb9d19f7df34536263a5074b35a5aba85df4
SickBow <mikecilona@gmail.com>
2023-08-10 21:17:29 -0400

    Added to BehaviorTrees namespace

--- Runtime/TreeConditionValue.cs
+++ Runtime/TreeConditionValue.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+namespace Sickbow.BehaviorTrees{
 [CreateAssetMenu(fileName = "TreeConditionValue", menuName = "BehaviorTree/TreeConditionValue")]
 public class TreeConditionValue : ScriptableObject
 {
     public bool value;
+}
 }
